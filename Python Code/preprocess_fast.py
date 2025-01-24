db0d31fe4087e20bacb16ae8fe512899005173e7
Steven VanOmmeren <sevanommeren@gmail.com>
2025-01-24 10:08:19 -0500

    a

--- Python Code/preprocess_fast.py
+++ Python Code/preprocess_fast.py
@@ -1,32 +1,33 @@
 from pypdf import PdfReader
-from chonkie import SentenceChunker
-from transformers import AutoTokenizer
 
-def setup_chunker(_chunk_size=256, _chunk_overlap=64, _min_sentences_per_chunk=1, tokenizer_model = "google-bert/bert-base-uncased"):
+# Faster chunker by approximating 1 word per 1 token. No tokenizer.
+
+def setup_chunker(_chunk_size=256, _chunk_overlap=64):
     # Logic to adjust inputs if necessary
-    if _chunk_size < 32:
-        _chunk_size = 32
-        print(f"Warning: chunk size too small, setting to {_chunk_size}.")
-
-    if 2 * _chunk_overlap >= _chunk_size:
+    if 2 * _chunk_overlap > _chunk_size:
         _chunk_overlap = round(_chunk_size / 2)
         print(f"Warning: chunk overlap too large, setting to {_chunk_overlap}.")
 
-    if _min_sentences_per_chunk < 1:
-        _min_sentences_per_chunk = 1
-        print(f"Warning: you tried to set minimum sentences to less than 1, setting to 1.")
-    
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_model)
+    def chunker(text):
+        # Split text into words
+        words = text.split(' ')
+        chunks = []
 
-    chunker = SentenceChunker(
-        tokenizer=tokenizer
-        , chunk_size=_chunk_size
-        , chunk_overlap=_chunk_overlap
-        , min_sentences_per_chunk=_min_sentences_per_chunk
-    )
-    # Need to play around with these settings to see what the appropriate chunk is
-    # Chunking too large would reduce accuracy, but chunking too small would generate too many vectors to search
-    # Also need to experiment with overlap
+        start = 0
+        end = _chunk_size-1
+
+        while end < len(words):
+            chunk = words[start:end]
+            chunks.append(chunk)
+
+            start = end - _chunk_overlap + 1
+            end = end + _chunk_overlap
+        
+        # make sure the last chunk has all the remaining words
+        if end < len(words) - 1:
+            chunks.append(words[end:])
+
+        return(chunks)
 
     return(chunker)
 
@@ -72,7 +73,7 @@ def preprocess(text):
     return(text)
 
 # Function to convert PDF to chunkable text
-def prepare_PDF(in_path, _chunk_size=256, _chunk_overlap=64, _min_sentences_per_chunk=1, tokenizer_model = "google-bert/bert-base-uncased"):
+def prepare_PDF(in_path, _chunk_size=256, _chunk_overlap=64):
     reader = PdfReader(in_path)
 
     # combine all pages into one list
@@ -88,13 +89,13 @@ def prepare_PDF(in_path, _chunk_size=256, _chunk_overlap=64, _min_sentences_per_
     paper_one_string = ' '.join(paper)
 
     # chunk the raw text
-    chunker = setup_chunker(_chunk_size, _chunk_overlap, _min_sentences_per_chunk, tokenizer_model)
-    chunks = chunker.chunk(paper_one_string)
+    chunker = setup_chunker(_chunk_size, _chunk_overlap)
+    chunks = chunker(paper_one_string)
 
     # Organize chunks and processed chunks into a dictionary
     chunk_data = {
-        'raw_chunk': [chunk.text for chunk in chunks]
-        , 'processed_chunk': [preprocess(chunk.text) for chunk in chunks]
+        'raw_chunk': [chunk for chunk in chunks]
+        , 'processed_chunk': [preprocess(chunk) for chunk in chunks]
         , 'file_path': [in_path for chunk in chunks]
     }
 
