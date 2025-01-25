222cac142d29e30e8aa2486c54014357171aac3c
Steven VanOmmeren <sevanommeren@gmail.com>
2025-01-25 18:09:35 -0500

    a

--- Python Code/preprocess.py
+++ Python Code/preprocess.py
@@ -4,6 +4,11 @@ import pymupdf
 
 def setup_chunker(_chunk_size=256, _chunk_overlap=64):
     # Logic to adjust inputs if necessary
+
+    # Option to treat the entire document as 1 chunk
+    if _chunk_size == False:
+        _chunk_size = 999999999
+
     if 2 * _chunk_overlap > _chunk_size:
         _chunk_overlap = round(_chunk_size / 2)
         print(f"Warning: chunk overlap too large, setting to {_chunk_overlap}.")
@@ -12,6 +17,9 @@ def setup_chunker(_chunk_size=256, _chunk_overlap=64):
         # Split text into words
         words = text.split(' ')
         chunks = []
+
+        if _chunk_size > len(words):
+            _chunk_size = len(words)
 
         start = 0
         end = _chunk_size-1
@@ -54,7 +62,7 @@ drop_words = [
     , "ABSTRACT"
 ]
 
-# Preprocessing function
+# Preprocessing function for PDFs
 def preprocess(text):
     # Connect words across lines
     text = text.replace('-\n', '')
@@ -75,6 +83,47 @@ def preprocess(text):
 
 # Function to convert PDF to chunkable text
 def prepare_PDF(in_path, _chunk_size=256, _chunk_overlap=64):
+    # Assert that the file is a PDF
+    assert in_path.lower().endswith('.pdf'), "This is not a PDF file. Use a different function."
+    
+    doc = pymupdf.open(in_path)
+
+    # combine all pages into one list
+    paper = []
+    for page in doc:
+        # extract text from page
+        page_text = page.get_text()
+
+        # append to paper
+        paper.append(page_text)
+
+    # convert list into string
+    paper_one_string = ' '.join(paper)
+
+    # chunk the raw text
+    chunker = setup_chunker(_chunk_size, _chunk_overlap)
+    chunks = chunker(paper_one_string)
+
+    # Organize chunks and processed chunks into a dictionary
+    chunk_data = {
+        'raw_chunk': [chunk for chunk in chunks]
+        , 'processed_chunk': [preprocess(chunk) for chunk in chunks]
+        , 'file_path': [in_path for chunk in chunks]
+    }
+
+    return(chunk_data)
+
+# Function to convert text files to chunkable text
+def prepare_text(in_path, _chunk_size=256, _chunk_overlap=64):
+    # Assert that the file is a text format
+    allowed_formats = ['.txt', '.r', '.do', '.py', '.sas', '.sql', '.vba']
+    
+    c=0
+    for i in allowed_formats:
+        if in_path.lower().endswith(i):
+            c+=1
+    assert c>0, "This is not a valid text file. Use a different function."
+    
     doc = pymupdf.open(in_path)
 
     # combine all pages into one list
