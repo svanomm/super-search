ef44585c61982fadc9230bd9dfb75b5990a57f57
Steven VanOmmeren <sevanommeren@gmail.com>
2025-01-27 21:49:43 -0500

    prep functions for database construction

--- Python Code/create_database.py
+++ Python Code/create_database.py
@@ -1,22 +1,77 @@
-from transformers import AutoTokenizer
 from sentence_transformers import SentenceTransformer
-from sentence_transformers.models import StaticEmbedding
+from tqdm import tqdm
 
-import os
-import sys
-import glob
+import os, sys, glob, pickle
 import pandas as pd
 import numpy as np
 import pynndescent as nn
 
-glob.glob(r"C:\Users\Steven\Desktop", "*.pdf")
-
 # Add the current directory to the path
 sys.path.append(os.getcwd())
-from preprocess import prepare_PDF
+from preprocess import prepare_PDF, prepare_text
 
-def prepare_filelist(file_path):
-    files = os.listdir(papers_repo)
+def prepare_filelist(filepath, allowed_text_types = ['.txt', '.r', '.do', '.py', '.sas', '.sql', '.vba']):
+    files = glob.glob(f"{filepath}/**/*", recursive=True)
+    
+    pdfs = [file for file in files if file.lower().endswith('.pdf')]
+    
+    t = []
+    for i in allowed_text_types:
+        t.append([file for file in files if file.lower().endswith(i)])
+    
+    texts = [ts for tss in t for ts in tss] # flatten the list of lists
+    
+    return {'pdfs': pdfs, 'texts': texts}
+
+def prepare_directory(file_path, backup_file = "chunking_backup", allowed_text_types = ['.txt', '.r', '.do', '.py', '.sas', '.sql', '.vba']):
+    files = prepare_filelist(file_path, allowed_text_types)
+
+    full_dict = {
+        'raw_chunk': []
+        , 'processed_chunk': []
+        , 'file_path': []
+    }
+
+    # Initiate the backup file
+    with open(f'./{backup_file}.pickle', 'wb') as f:
+        pickle.dump(full_dict, f)
+
+    iters_per_dump = 20
+    counter = 1
+    
+    # Process PDFs
+    if len(files['pdfs']) > 0:
+        for file in tqdm(files['pdfs'], desc = "Chunking PDFs"):
+            iter_dict = prepare_PDF(file)
+            full_dict['raw_chunk'].extend(iter_dict['raw_chunk'])
+            full_dict['processed_chunk'].extend(iter_dict['processed_chunk'])
+            full_dict['file_path'].extend(iter_dict['file_path'])
+
+            counter+=1
+
+            # Backup the data
+            if counter % iters_per_dump == 0:
+                with open(f'./{backup_file}.pickle', 'wb') as f:
+                    pickle.dump(full_dict, f)
+    
+    # Process text files
+    if len(files['texts']) > 0:
+        for file in tqdm(files['texts'], desc = "Chunking Text Files"):
+            iter_dict = prepare_text(file)
+            full_dict['raw_chunk'].extend(iter_dict['raw_chunk'])
+            full_dict['processed_chunk'].extend(iter_dict['processed_chunk'])
+            full_dict['file_path'].extend(iter_dict['file_path'])
+            
+            counter+=1
+
+            # Backup the data
+            if counter % iters_per_dump == 0:
+                with open(f'./{backup_file}.pickle', 'wb') as f:
+                    pickle.dump(full_dict, f)
+
+    assert len(full_dict['raw_chunk']) > 0, "Found no files to analyze."
+
+    return(full_dict)
 
 # Current encoding model implementation: static-retrieval-mrl-en-v1
 # https://huggingface.co/sentence-transformers/static-retrieval-mrl-en-v1
@@ -28,45 +83,31 @@ model = SentenceTransformer(
     , truncate_dim=truncated_dimensions
     )
 
-## TESTING
-# Importing a lot of PDFs to see how long this takes
-papers_repo = r"C:\Users\Steven\Documents\Python\Data\NBER papers"
+def create_database(file_path
+                    , model_name = "sentence-transformers/static-retrieval-mrl-en-v1"
+                    , truncated_dimensions = 1024
+                    , log_file = "chunking_log.txt"
+                    , allowed_text_types = ['.txt', '.r', '.do', '.py', '.sas', '.sql', '.vba']
+                    ):
+    # Define the model
+    model = SentenceTransformer(
+        model_name
+        , device="cpu"
+        , truncate_dim=truncated_dimensions
+        )
+    
+    # Prepare the directory
+    print("Importing and processing files...")
+    full_dict = prepare_directory(file_path, log_file, allowed_text_types)
 
-files = os.listdir(papers_repo)
-files.sort(reverse=True)
+    # Encode the chunks
+    print("Encoding the text...")
+    vecs = model.encode(full_dict['processed_chunk'])
+    full_dict['vector'] = [i for i in np.unstack(vecs)]
+    
+    # Create the nearest-neighbor index
+    print("Creating the nearest-neighbor index...")
+    index = nn.NNDescent(vecs)
+    index.prepare() # preloads the operations so that future uses are faster
 
-full_dict = {
-    'raw_chunk': []
-    , 'processed_chunk': []
-    , 'file_path': []
-}
-
-counter=1
-
-for paper in files[0:100]:
-    f = f"{papers_repo}/{paper}"
-    iter_dict = prepare_PDF(f)
-    full_dict['raw_chunk'].extend(iter_dict['raw_chunk'])
-    full_dict['processed_chunk'].extend(iter_dict['processed_chunk'])
-    full_dict['file_path'].extend(iter_dict['file_path'])
-    if counter%5==0:
-        print(f"Finished file {counter}.")
-    counter+=1
-
-df = pd.DataFrame.from_dict(full_dict)
-df
-
-# Currently takes around 1 second per file (with tokenization chunking)
-# Takes ~ 0.7 seconds with approximate chunking
-# After switching to PyMuPDF, 0.1 seconds per file, but 3 cmsOpenProfileFromMem errors
-
-vecs = model.encode(df['processed_chunk'])
-# This returns a np array of shape (n, d), where n is 
-#     number of chunks and d is embedding dimensions.
-
-df['vector'] = [i for i in np.unstack(vecs)]
-# Add the embeddings to our dataframe in a single variable,
-#     so each cell contains the d-dimensional np vector.
-
-df
-# takes 3-4 seconds
+    return(full_dict, index)
